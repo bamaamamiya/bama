6c2942ef04db47519ba7986e06ebd1115747fbcf
BaldiMaulana <m.baldimaulana.com>
2024-07-18 00:42:23 +0700

    system update

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -48,26 +48,39 @@ const Book =() =>{
             status: 'CURRENTLY READING',
             author: 'Roger J. Hamilton'
         },
+        {
+            imgBook: "https://images.tokopedia.net/img/cache/700/hDjmkQ/2023/11/20/8744a5ca-e873-4bea-a242-d2500f0d1454.jpg",
+            title: "The Science of Self-Learning",
+            status: 'COMPLETE',
+            author: 'Peter Hollins'
+        },
     ];
 
     const filteredBooks = filter === 'ALL' ? BookList : BookList.filter(book => book.status === 'COMPLETE');
+    const complatedBooks = BookList.filter(book => book.status === 'COMPLETE').length
+    const currentlyBooks = BookList.filter(book => book.status === 'CURRENTLY READING').length
 
 
     return(
         <div className='h-max'>
             <div className='grid justify-center items-center'>
-        <div className="text-center ">
+        <div className="text-center space-y-2 space-x-2">
             <button
                 onClick={() => setFilter(filter === 'ALL' ? 'COMPLETE' : 'ALL')}
                 className="bg-primary p-2 rounded-md font-bold border-2 border-r-4 border-b-4"
             >
                 {filter === 'ALL' ? 'Show Completed' : 'Show All'}
             </button>
+
+            <div className='flex justify-center items-center gap-2'>
+            <p className='text-xl'>Completed Books : {complatedBooks} </p>
+            <p className='text-xl'>Currently Reading : {currentlyBooks} </p>
+            </div>
         </div>
         <div className='grid justify-center text-center gap-2 m-4 md:grid md:grid-cols-2 lg:grid-cols-3 grid-cols-2'>
             {filteredBooks.map((book, index) => (
                 <div key={index}  className='grid justify-center border-2 p-2 rounded-lg'>
-                        <img src={book.imgBook} alt={book.title} className=' h-[300px] w-full rounded-lg'/>
+                        <img src={book.imgBook} alt={book.title} className='md:h-[320px] w-full rounded-lg'/>
                     <h2 className="text-lg font-bold">{book.title}</h2>
                     <h3 className="text-sm">{book.author}</h3>
                     <p className="font-extrabold text-xl">{book.status}</p>
