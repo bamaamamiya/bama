d24b9646e4a32c55f5279bd35205f86ceea26239
BaldiMaulana <m.baldimaulana.com>
2024-07-22 22:40:38 +0700

    system update

--- src/components/Quo/Mess.jsx
+++ src/components/Quo/Mess.jsx
@@ -1,7 +1,7 @@
 const Mess =() =>{
     return(
-        <div className="bg-black text-white h-screen flex justify-center items-center p-2">
-            <h1 className="text-center text-xl font-extralight italic p-2 ">" your thought lead to action,action determine habit,habit determine patterns of behavior,and your behavior determines the outcome of your life "</h1>
+        <div className="bg-black text-white h-screen flex justify-center items-center p-2 ">
+            <h1 className="text-center text-xl font-extralight italic p-2 w-80">" your thought lead to action,action determine habit,habit determine patterns of behavior,and your behavior determines the outcome of your life "</h1>
         </div>
     )
 }
