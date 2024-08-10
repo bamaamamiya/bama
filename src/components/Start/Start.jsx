65e914f898617feb4b608e6a7528645b8a4e9a5d
BaldiMaulana <m.baldimaulana.com>
2024-08-11 02:05:44 +0700

    system update

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -6,7 +6,7 @@ const Start = () => {
         <div className="bg-black text-white h-screen flex justify-center items-center ">
             <Link to='/profil' >
                 <img 
-                src='https://raw.githubusercontent.com/bamaamamiya/bama/master/public/images/player.webp' 
+                src='/images/player.webp' 
                 alt="player-start" 
                 width="564" 
                 height="376" />
