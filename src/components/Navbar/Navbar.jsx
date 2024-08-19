d6412e755b3d9433f54b9bb99ceb5dbe77c0f0f9
BaldiMaulana <m.baldimaulana.com>
2024-08-19 23:48:06 +0700

    system update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,12 +35,6 @@ function Navbar() {
       <div className={`sm:flex sm:gap-4 m-3 text-2xl ${isMenuOpen ? 'block' : 'hidden'}`}>
         <ul className="sm:flex sm:gap-4 sm:ml-auto border-b-2 border-t-2 md:border-none">
           <li className='text-end'>
-            <Link to='/quest'>Quest</Link>
-          </li>
-          <li className='text-end'>
-            <Link to='/book'>Book</Link>
-          </li>
-          <li className='text-end'>
             <Link to='/player'>Player</Link>
           </li>
           <li className='text-end'>
@@ -52,6 +46,14 @@ function Navbar() {
           <li className='text-end'>
             <Link to='/'>StoryMode</Link>
           </li>
+
+          <li className='text-end'>
+            <Link to='/quest'>Quest</Link>
+          </li>
+          <li className='text-end'>
+            <Link to='/book'>Book</Link>
+          </li>
+
         </ul>
       </div>
     </nav>
